0dc4d19d5cbc98356b2275a7e480c31bde478869
Ishimwe-delice <ishimwedelice12@gmail.com>
2024-11-13 05:58:03 -0800

    Add files via upload

---
+++ style.js
@@ -0,0 +1,6 @@
+// Placeholder for future JavaScript functionality
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', function() {
+        alert('Added to cart!');
+    });
+});
